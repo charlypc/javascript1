cd8aed1e7a5ced8c9175cf3a27b333caea4144c7
CharlyP <83354090+charlypc@users.noreply.github.com>
2022-10-11 01:07:40 +0200

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,77 @@
+let myName = "Charly";
+let myAge = 37;
+
+console.log(myName);
+
+let mySistersName = true;
+console.log(mySistersName);
+
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+console.log(sum);
+
+let firstName = "Charly";
+let surname = "Adolfus";
+
+let fullName = firstName + surname;
+console.log(fullName);
+
+console.log(firstName.concat(" ", surname));
+
+let fullname = "Charly" + "  " + "Adolfus";
+console.log(fullname);
+
+let age = 30;
+
+if (age >= 30) {
+  console.log("He is getting old");
+} else {
+  console.log("He is still a child");
+}
+
+let height = 17;
+
+if (height < 17) {
+  console.log("His height is less than 17");
+} else if (height >= 17 && height <= 20) {
+  console.log(" His height is between 17 and 20.");
+} else {
+  console.log("His height is over 20");
+}
+
+var car = {
+  carMake: "Citreon",
+  model: "c3 picasso",
+  year: 2015,
+  color: "Red",
+  fuelType: "Petrol",
+};
+
+console.log(car.carMake);
+console.log(car.year);
+
+let hisData = {
+  hisName: "Daniel",
+  age: "27",
+  height: "176cm",
+};
+
+console.log(hisData.age);
+
+let color = [
+  "Red",
+  "Blue",
+  "Purple",
+  "Green",
+  "Black",
+  "Violet",
+  "Indigo",
+  "Brown",
+];
+
+console.log(color[0]);
+console.log(color[1]);
+console.log(color[4]);
+console.log(color[2]);
